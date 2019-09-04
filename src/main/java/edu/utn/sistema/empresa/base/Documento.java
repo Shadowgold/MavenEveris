9560d713b119f20a3d2ca79ed0e62322a4de1ac8
PC <PC@192.168.45.116>
2019-09-04 12:09:56 -0300

    creacion de la clase Documento con sus atributos, constructores, Getter and Setter

--- src/main/java/edu/utn/sistema/empresa/base/Documento.java
+++ src/main/java/edu/utn/sistema/empresa/base/Documento.java
@@ -5,7 +5,37 @@ import edu.utn.sistema.usuarios.Persona;
 
 public class Documento {
     
+    //Atributos
     private Persona persona;
     private int Dni;
+    
+    
+    //Constructor por Defecto
+    public Documento() {
+    }
+    
+    //Cosntructor Sobrecargado
+    public Documento(Persona persona, int Dni) {
+        this.persona = persona;
+        this.Dni = Dni;
+    }
+    
+    
+    //Getter and Setter
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    public int getDni() {
+        return Dni;
+    }
+
+    public void setDni(int Dni) {
+        this.Dni = Dni;
+    }
     
 }
