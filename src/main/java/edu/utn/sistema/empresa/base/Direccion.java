ce69e4cd254756f4e07d15e9e1d377a3966fb956
PC <PC@192.168.45.116>
2019-09-04 11:04:53 -0300

    Creamos y Agregamos el paquete Base con sus respectivas clases.

--- src/main/java/edu/utn/sistema/empresa/base/Direccion.java
+++ src/main/java/edu/utn/sistema/empresa/base/Direccion.java
@@ -4,6 +4,10 @@ package edu.utn.sistema.empresa.base;
 
 public class Direccion {
     
+    private Pais pais;
+    private String calle;
+    private String numero;
+    private String localidad;
     
     
 }
