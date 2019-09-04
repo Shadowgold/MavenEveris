0df29f3ed0059755dd8e28f8b127911ad3ba5a84
hernanvidela <hernan11.08.10@gmail.com>
2019-09-04 12:05:55 -0300

    Creacion de atributos y contructores de la entidad Movimientos_CtaCte

--- src/main/java/edu/utn/sistema/cuentas/Movimientos_CtaCte.java
+++ src/main/java/edu/utn/sistema/cuentas/Movimientos_CtaCte.java
@@ -5,6 +5,8 @@
  */
 package edu.utn.sistema.cuentas;
 
+import edu.utn.sistema.usuarios.Cliente;
+
 /**
  *
  * @author fabri
@@ -20,6 +22,81 @@ public class Movimientos_CtaCte {
     }
     
     
+    
+    private String tipoMovimiento;
+    
+    private String descripcionMovimiento;
+    
+    private Cliente cliente;
+
+    public Movimientos_CtaCte() {
+    }
+
+    public Movimientos_CtaCte(Cuentas cuenta, String tipoMovimiento, String descripcionMovimiento, Cliente cliente) {
+        this.cuenta = cuenta;
+        this.tipoMovimiento = tipoMovimiento;
+        this.descripcionMovimiento = descripcionMovimiento;
+        this.cliente = cliente;
+    }
+    
+    
+
+    /**
+     * Get the value of cliente
+     *
+     * @return the value of cliente
+     */
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    /**
+     * Set the value of cliente
+     *
+     * @param cliente new value of cliente
+     */
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+
+    /**
+     * Get the value of descripcionMovimiento
+     *
+     * @return the value of descripcionMovimiento
+     */
+    public String getDescripcionMovimiento() {
+        return descripcionMovimiento;
+    }
+
+    /**
+     * Set the value of descripcionMovimiento
+     *
+     * @param descripcionMovimiento new value of descripcionMovimiento
+     */
+    public void setDescripcionMovimiento(String descripcionMovimiento) {
+        this.descripcionMovimiento = descripcionMovimiento;
+    }
+
+
+    /**
+     * Get the value of tipoMovimiento
+     *
+     * @return the value of tipoMovimiento
+     */
+    public String getTipoMovimiento() {
+        return tipoMovimiento;
+    }
+
+    /**
+     * Set the value of tipoMovimiento
+     *
+     * @param tipoMovimiento new value of tipoMovimiento
+     */
+    public void setTipoMovimiento(String tipoMovimiento) {
+        this.tipoMovimiento = tipoMovimiento;
+    }
+
     
     public Cuentas getCuenta() {
         return cuenta;
