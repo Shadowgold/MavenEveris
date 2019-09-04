9a6fd74876a9aded55ccba617d2dbe432daf30ea
Martin-Ruggeri <martinruggeri.00@gmail.com>
2019-09-04 12:12:26 -0300

    aaa

--- src/main/java/edu/utn/sistema/empresa/base/Documento.java
+++ src/main/java/edu/utn/sistema/empresa/base/Documento.java
@@ -7,5 +7,22 @@ public class Documento {
     
     private Persona persona;
     private int Dni;
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    public int getDni() {
+        return Dni;
+    }
+
+    public void setDni(int Dni) {
+        this.Dni = Dni;
+    }
+    
     
 }
