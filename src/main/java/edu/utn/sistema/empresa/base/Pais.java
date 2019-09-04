cfbaa882b621a260f53777eb11636c5ce66cba8d
PC <PC@192.168.45.116>
2019-09-04 11:13:12 -0300

    Creacion de la clase Clase Pais con sus respectivos atributos y metodos

--- src/main/java/edu/utn/sistema/empresa/base/Pais.java
+++ src/main/java/edu/utn/sistema/empresa/base/Pais.java
@@ -4,4 +4,23 @@ package edu.utn.sistema.empresa.base;
 
 public class Pais {
     
+    private String nombrePais;
+
+    public Pais() {
+    }
+
+    public Pais(String nombrePais) {
+        this.nombrePais = nombrePais;
+    }
+
+    public String getNombrePais() {
+        return nombrePais;
+    }
+
+    public void setNombrePais(String nombrePais) {
+        this.nombrePais = nombrePais;
+    }
+    
+    
+    
 }
