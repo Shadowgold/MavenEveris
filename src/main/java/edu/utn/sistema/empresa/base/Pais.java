a59ced80c239f07aef3e156c5c30418c913fce1f
PC <PC@192.168.45.116>
2019-09-04 12:06:58 -0300

    creacion de la clase Pais con sus atributos, constructores, Getter and Setter

--- src/main/java/edu/utn/sistema/empresa/base/Pais.java
+++ src/main/java/edu/utn/sistema/empresa/base/Pais.java
@@ -4,15 +4,22 @@ package edu.utn.sistema.empresa.base;
 
 public class Pais {
     
+    //Atributos
     private String nombrePais;
-
+    
+    
+    //Constructor por Defecto
     public Pais() {
     }
-
+    
+    
+    //Constructor Sobrecargado
     public Pais(String nombrePais) {
         this.nombrePais = nombrePais;
     }
-
+    
+    
+    //Get and Set
     public String getNombrePais() {
         return nombrePais;
     }
