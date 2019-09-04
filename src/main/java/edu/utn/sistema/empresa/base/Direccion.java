2ff1a3f6bcb8dc9f4edeaf037e0bf127dfe3030d
PC <PC@192.168.45.116>
2019-09-04 11:12:39 -0300

    Creacion de la clase Clase Direccion con sus respectivos atributos y metodos

--- src/main/java/edu/utn/sistema/empresa/base/Direccion.java
+++ src/main/java/edu/utn/sistema/empresa/base/Direccion.java
@@ -8,6 +8,50 @@ public class Direccion {
     private String calle;
     private String numero;
     private String localidad;
+
+    public Direccion() {
+    }
+
+    public Direccion(Pais pais, String calle, String numero, String localidad) {
+        this.pais = pais;
+        this.calle = calle;
+        this.numero = numero;
+        this.localidad = localidad;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+    
+    
     
     
 }
